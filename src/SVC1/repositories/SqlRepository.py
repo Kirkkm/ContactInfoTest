88e6e017a06b0f746cd45b283fad4e348f124120
root <root@Kirk-Laptop>
2025-01-17 11:21:24 -0500

    fixing error found in SqlRepository

--- src/SVC1/repositories/SqlRepository.py
+++ src/SVC1/repositories/SqlRepository.py
@@ -1,28 +1,30 @@
 import mysql.connector
 from mysql.connector import Error
 
+
 class SqlRepository:
-    def __init__(self, hostname: str, username: str, userPass: str, db: str):
-        connection = None
+    def __init__(self, hostname: str, username: str, password: str, db: str):
         try:
-            connection = mysql.connector.connect(host = hostname, user = username, password = userPass, database = db)
-            print(f'successfully connected to {hostname}')
+            connection = mysql.connector.connect(
+                host=hostname, user=username, password=password, database=db
+            )
+            print(f"successfully connected to {hostname}")
         except Error as e:
-            print('encountered error')
-            print('=================')
+            print("encountered error")
+            print("=================")
             print(e)
         self.connection = connection
 
-    def insert(self, insertStatement):
+    def insert(self, insertStatement: str) -> None:
         cursor = self.connection.cursor()
         cursor.execute(insertStatement)
         self.connection.commit()
 
-    def checkTable(self, table: str) -> bool:
-        cursor = self.connection.cursor()
-        tableCursor = cursor.execute('SHOW TABLES')
-        return table in tableCursor
-    
-    def createTable(self, statement: str) -> void:
+    def table_exist(self, table: str) -> bool:
+        table_cursor = self.connection.cursor()
+        table_cursor.execute("SHOW TABLES")
+        return table in table_cursor
+
+    def create_table(self, statement: str) -> None:
         cursor = self.connection.cursor()
         cursor.execute(statement)
